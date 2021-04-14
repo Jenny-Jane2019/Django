0305a28cedf34b08fbb57032deb957e92325ff39
Jenny-Jane2019 <61904048+Jenny-Jane2019@users.noreply.github.com>
2021-04-14 11:38:12 +0800

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -75,14 +75,14 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
-}
+}'''
 
-'''DATABASES = {
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':  'mydb',
@@ -91,7 +91,7 @@ DATABASES = {
         'HOST': 'localhost',
         'PORT': '3306',
     }
-}'''
+}
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
