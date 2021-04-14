9addb99f1042e443d323fb20a451d07afb044134
Jenny-Jane2019 <61904048+Jenny-Jane2019@users.noreply.github.com>
2021-04-14 17:15:12 +0800

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -86,8 +86,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME':  'mydb',
-        'USER':  'root',
-        'PASSWORD': 'django123',
+        'USER':  'lqj',
+        'PASSWORD': 'test123456',
         'HOST': 'localhost',
         'PORT': '3306',
     }
